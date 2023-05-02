dbc0da48afc350cce5badb8caae32d38a2fb5378
Felicia Suryawinata <fsuryawinata@student.unimelb.edu.au>
2023-05-02 16:51:03 +1000

    Changed utility to place in closest opponent piece

--- agent/minimax.py
+++ agent/minimax.py
@@ -4,7 +4,8 @@ from typing import List
 
 BOARD_SIZE = 7
 DIRECTIONS = [HexDir.Up, HexDir.UpRight, HexDir.UpLeft, HexDir.Down, HexDir.DownLeft, HexDir.DownRight]
-
+UNDOMIN = 0
+UNDOMAX = 0
 
 def minimaxDecision(depth, game, colour):
     """
@@ -16,8 +17,8 @@ def minimaxDecision(depth, game, colour):
     for op in operators:
         game.apply_action(op)
         state = game
-        game.undo_action()
         value = minimaxValue(state, game, depth, colour)
+        game.undo_action()
         if value > best_value:
             best_value = value
             best_operator = op
@@ -29,23 +30,22 @@ def getOperators(game) -> List[Action]:
     """
     Find all valid moves
     """
-    # List possible SPAWN actions
-    empty_cells = [pos for pos in game._state.keys() if not game._cell_occupied(pos)]
-    for r in range(BOARD_SIZE):
-        for q in range(BOARD_SIZE):
-            if not game._cell_occupied(HexPos(r, q)):
-                empty_cells.append(HexPos(r, q))
+    opponent_cells = getOpponentCells(game)
+
+    # List possible SPAWN actions within 2 moves of opponent cells
+    empty_cells = []
+    for pos, power in opponent_cells.items():
+        neighbour_opponent = getNeighbours(pos, power)
+
+    for pos in neighbour_opponent:
+        if not game._cell_occupied(pos):
+            empty_cells.append(pos)
     spawn_actions = [SpawnAction(pos) for pos in empty_cells]
 
     # List possible SPREAD actions
-    player_cells = []
+    player_cells = getPlayerCells(game)
     spread_actions = []
-    power = []
 
-    for pos, state in game._state.items():
-        if state.player == game._turn_color:
-            player_cells.append(pos)
-            power.append(state.power)
 
     for pos in player_cells:
         for direction in DIRECTIONS:
@@ -69,31 +69,29 @@ def minimaxValue(state, game, depth, player_colour):
 
 
 def maxValue(state, game, depth, alpha, beta):
-    if state.game_over | depth == 0:
+    if state.game_over or depth == 0:
         return utility(state)
 
     best_score = float('-inf')
     operators = getOperators(game)
-    # print(f"GAME STATE {game._state.values()}")
+
     for op in operators:
-        # print(f"TOTAL POWER {game._total_power}")
         game.apply_action(op)
         new_state = game
-        game.undo_action()
         score = minValue(new_state, game, depth - 1, alpha, beta)
+        game.undo_action()
+
         best_score = max(best_score, score)
-        # print(f"TOTAL POWER {game._total_power}")
 
         if best_score >= beta:
             return best_score
 
         alpha = max(alpha, best_score)
-
     return best_score
 
 
 def minValue(state, game, depth, alpha, beta):
-    if state.game_over | depth == 0:
+    if state.game_over or depth == 0:
         return utility(state)
 
     best_score = float('inf')
@@ -102,19 +100,108 @@ def minValue(state, game, depth, alpha, beta):
     for op in operators:
         game.apply_action(op)
         new_state = game
-        game.undo_action()
         score = maxValue(new_state, game, depth - 1, alpha, beta)
+        game.undo_action()
         best_score = min(best_score, score)
 
-        if best_score >= beta:
+        if best_score <= alpha:
             return best_score
 
         beta = min(beta, best_score)
 
     return best_score
 
+def getPlayerCells(game):
+    player_cells = {}
+    for pos, state in game._state.items():
+        if state.player == game._turn_color:
+            player_cells[pos] = state.power
+    return player_cells
+
+def getOpponentCells(game):
+    opponent_cells = {}
+    for pos, state in game._state.items():
+        if state.player != game._turn_color and state.player != None:
+            opponent_cells[pos] = state.power
+    return opponent_cells
+
+def checkCapture(pos, direction, pow, opponent_pieces):
+    """
+    Check if SPREAD action captures any opponent pieces
+    """
+    i = 1
+    while i <= pow:
+
+        newq = pos.q + i * direction.q
+        newr = pos.r + i * direction.r
+
+        if 0 > newq or newq > 6:
+            newq = newq % BOARD_SIZE
+        if 0 > newr or newr > 6:
+            newr = newr % BOARD_SIZE
+
+
+        pos = HexPos(newq, newr)
+        if pos in opponent_pieces:
+            return 1
+        i += 1
+    return 0
 
 def utility(state):
+    """
+    Calculate the utility value of the given state for the given player and
+    return a numeric value representing the utility
+    """
+    player_pieces = getPlayerCells(state)
+    opponent_pieces = getOpponentCells(state)
+
+    for pos in player_pieces:
+        if isWithin2Moves(pos, opponent_pieces):
+            return 1
+
+    # # Possible SPREAD action can capture opponent's token
+    # for pos in player_pieces:
+    #     for direction in DIRECTIONS:
+    #         pow = state._state[pos].power
+    #         if checkCapture(pos, direction, pow, opponent_pieces):
+    #             return 1
+
+    return -1
+
+def isWithin2Moves(pos, opponent_pieces):
+    for opponent_pos in opponent_pieces:
+        if getDistance(pos, opponent_pos) <= 2:
+            return True
+    return False
+
+def getDistance(pos1, pos2):
+    """
+    Get the distance between two hexagonal positions
+    """
+    q1, r1 = pos1.q, pos1.r
+    q2, r2 = pos2.q, pos2.r
+    return (abs(q1 - q2) + abs(q1 + r1 - q2 - r2) + abs(r1 - r2)) // 2
+
+def getNeighbours(cell, power):
+    """
+    Get surrounding cells
+    """
+    power = power + 1
+    neighbours = []
+    for dir in DIRECTIONS:
+        newq = cell.q + power * dir.q
+        newr = cell.r + power * dir.r
+
+        if 0 > newq or newq > 6:
+            newq = newq % BOARD_SIZE
+        if 0 > newr or newr > 6:
+            newr = newr % BOARD_SIZE
+
+        neighbours.append(HexPos(newr, newq))
+
+    return neighbours
+
+def utilitypower(state):
     # Calculate the utility value of the given state for the given player
     # Return a numeric value representing the utility
     red_pow = 0
